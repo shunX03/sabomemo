90d816fb6cecc6c1c6d58d4120c9ed07f21adfe0
EC2 Default User <you@example.com>
2022-12-07 09:21:38 +0000

    ajax(非同期通信)

--- app/controllers/public/likes_controller.rb
+++ app/controllers/public/likes_controller.rb
@@ -3,14 +3,15 @@ class Public::LikesController < ApplicationController
     post = Post.find(params[:post_id])
     post = current_user.likes.new(post_id: post.id)
     post.save
-    redirect_to request.referer
   end
 
   def destroy
     post = Post.find(params[:post_id])
     post = current_user.likes.find_by(post_id: post.id)
     post.destroy
-    redirect_to request.referer
   end
 
+  def post_params
+   @post = Post.find(params[:id])
+  end
 end
